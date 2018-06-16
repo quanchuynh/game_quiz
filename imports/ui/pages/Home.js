4bca5f6b93d5429950372b073f5aa3c41de5f127
Quan C. Huynh <quanchuynh@gmail.com>
2018-06-16 07:58:01 -0500

    Add images for home page

--- imports/ui/pages/Home.js
+++ imports/ui/pages/Home.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './Home.css';
+import RotatingBackgroundImage from '../components/RotatingBackgroundImage';
 
 class Home extends Component {
   render() {
+    let textStyle = {position: "absolute", top: "30px", textAlign: "center", 
+                     marginLeft: "10%", marginRight: "auto"};
     return (
       <div className="homePage">
-        <h1>Welcome to the Encyclopedic Brain</h1>
-        <h4> Have Fun, Play</h4>
-        <h4> Expand Your Knowledge, Play</h4>
-        <h4> Gain Intellectual Satisfaction, Play</h4>
-        <h4> Earn Money, Play</h4>
+        <RotatingBackgroundImage/>
+        <div style={textStyle}>
+          <h1>Welcome to the Encyclopedic Brain</h1>
+          <h4> Have Fun, Play</h4>
+          <h4> Expand Your Knowledge, Play</h4>
+          <h4> Gain Intellectual Satisfaction, Play</h4>
+          <h4> Earn Money, Play</h4>
+        </div>
       </div>
     );
   }
